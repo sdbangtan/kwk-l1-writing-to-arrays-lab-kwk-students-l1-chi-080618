d13f45b6b7b748e217f619344ebbb7567f72ad43
Saumya Malhotra <ideagem1@gmail.com>
2018-08-08 16:58:13 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,15 +4,25 @@ rainbow_colors = [
   "light_cyan"
 ]
 
+# def change_rainbow_colors
+#   rainbow_colors=["red", "light_red", "light_yellow"]
+#   return rainbow_colors
+# end
+
+# def add_colors
+#   rainbow_colors=["red", "light_red", "light_yellow"]
+#   rainbow_colors<<("green")
+#   rainbow_colors<<("blue")
+#   return rainbow_colors
+# end
+
 def change_rainbow_colors
-  rainbow_colors=["red", "light_red", "light_yellow"]
-  return rainbow_colors
+  return rainbow_colors=["red", "light_red", "light_yellow"]
 end
 
 def add_colors
-  rainbow_colors=["red", "light_red", "light_yellow"]
-  rainbow_colors<<("green")
-  rainbow_colors<<("blue")
+  rainbow_colors=change_rainbow_colors
+  rainbow_colors.push("green")
+  rainbow_colors.push("blue")
   return rainbow_colors
 end
-
